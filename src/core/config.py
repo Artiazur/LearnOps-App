fb10d7580937110ea0d8cc096299028d8b0c7056
Artiazur <rmyrainbow@gmail.com>
2026-06-19 17:28:46 +0330

    feat(config): add JWT and token settings

--- src/core/config.py
+++ src/core/config.py
@@ -1,25 +1,34 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
 
 
 class Settings(BaseSettings):
-    db_user: str
-    db_password: str
-    db_host: str
-    db_port: int
-    db_name: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_MINUTES: int
+    PRIVATE_KEY_PATH: str
+    PUBLIC_KEY_PATH: str
+    ALGORITHM: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
     @property
     def async_database_url(self) -> str:
         return (
-            f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
-        
+
     @property
     def sync_database_url(self) -> str:
         return (
-            f"postgresql+psycopg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+            f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
-
 settings = Settings()
+
+PRIVATE_KEY = Path(settings.PRIVATE_KEY_PATH).read_text()
+PUBLIC_KEY = Path(settings.PUBLIC_KEY_PATH).read_text()
+

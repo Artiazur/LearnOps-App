6be47f50958092d148b515d76c88da7db4b05beb
Artiazur <rmyrainbow@gmail.com>
2026-08-08 20:32:59 +0330

    refactor: replace password validation library

--- backend/src/modules/user/validators/password_validator.py
+++ backend/src/modules/user/validators/password_validator.py
@@ -1,9 +1,11 @@
-from zxcvbn import zxcvbn
+from password_strength import PasswordPolicy
 
+policy = PasswordPolicy.from_names(
+    length=8,
+    uppercase=1,
+    numbers=1,
+    special=1,
+)
 
-def is_password_valid(password: str):
-    result = zxcvbn(password)
-    score = result["score"]
-    if score >= 2:
-        return True
-    return False
+def is_password_valid(password: str) -> bool:
+    return len(policy.test(password)) == 0
